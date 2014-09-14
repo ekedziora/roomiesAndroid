a3e73137bf1ad08cb736bca339705684d58f5ff4
ekedziora <ekedziora13@gmail.com>
2014-09-14 15:34:06 +0200

    Pierwszy commit Android Studio

---
+++ app/src/main/java/pl/kedziora/emilek/roomies/activities/LoginActivity.java
@@ -0,0 +1,71 @@
+package pl.kedziora.emilek.roomies.activities;
+
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+import pl.kedziora.emilek.roomies.R;
+
+public class LoginActivity extends Activity {
+
+    private static final int GET_ACCOUNT_CODE = 1001;
+
+    String accountName;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        findViewById(R.id.login_loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Accounts", Joiner.on(", ").join(getAccountNames()));
+                Intent intent = AccountPicker.newChooseAccountIntent(null, null, new String[]{"com.google"},
+                        false, null, null, null, null);
+                startActivityForResult(intent, GET_ACCOUNT_CODE);
+                Log.i("Accounts", accountName);
+            }
+        });
+    }
+
+    private List<String> getAccountNames() {
+        AccountManager accountManager = AccountManager.get(this);
+        Account[] accounts = accountManager.getAccountsByType(
+                GoogleAuthUtil.GOOGLE_ACCOUNT_TYPE);
+        List<String> names = Lists.newArrayListWithExpectedSize(accounts.length);
+        for (Account account : accounts) {
+            names.add(account.name);
+        }
+        return names;
+    }
+
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode,
+                                    final Intent data) {
+        if (requestCode == GET_ACCOUNT_CODE && resultCode == RESULT_OK) {
+            accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.login, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
