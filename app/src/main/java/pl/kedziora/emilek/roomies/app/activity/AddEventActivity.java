2aab49bbba74750595e53a8f0e6cca611125a638
Emil Kędziora <ekedziora@opi.org.pl>
2014-11-28 16:06:53 +0100

    Add event activity

---
+++ app/src/main/java/pl/kedziora/emilek/roomies/app/activity/AddEventActivity.java
@@ -0,0 +1,15 @@
+package pl.kedziora.emilek.roomies.app.activity;
+
+public class AddEventActivity extends BaseActivity {
+
+
+    @Override
+    public void proceedData() {
+
+    }
+
+    @Override
+    public void sendRequest() {
+
+    }
+}
